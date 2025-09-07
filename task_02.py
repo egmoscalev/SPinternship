08ea4aa34f5c35b6213f3109a58331d25878c230
GeorgyMoscalev <92420326+egmoscalev@users.noreply.github.com>
2025-09-07 08:57:11 +0300

    Update task_02.py

--- task_02.py
+++ task_02.py
@@ -1,6 +1,8 @@
 def coincidence (mylist,myrange):
   newlist = []
   for i in mylist:
-    if i in myrange:
-      newlist.append(i)
+    if isinstance(i,int) or isinstance (i,float):
+      if i >= myrange[0] and i <= myrange[-1]:
+        newlist.append(i)
+  
   return newlist

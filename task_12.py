9f460634a475f357d89e4beef898f533ab270a0e
egmoscalev <vivalaamer@gmail.com>
2025-11-08 18:12:17 +0300

    modified:   task_12.py

--- task_12.py
+++ task_12.py
@@ -12,33 +12,26 @@ class Dessert:
         self.set_calories(calories)
     
     def set_name(self,v):
-        if not isinstance(v, str):
-            if v != None:
-                print("Name should be a string. Name set to None")
-            self._name = None
-        else:
-            self._name = v
+        self.name = v
     
     def get_name(self):
-      return self._name
+      return self.name
     
     def set_calories(self, v):
-        if not isinstance(v, int or float) or v < 0:
-            if v != None:
-                print("Calories should be a non-negative number. Calories set to 0")
-            self._calories = 0
-        else:
-            self._calories = v
+        self.calories = v
     
     def get_calories(self):
-      return self._calories
+      return self.calories
         
 
     def is_healthy(self):
-        if (self._calories < 200):
-            return True
-        
-        return False
+        try:
+            if (self.calories < 200):
+                return True
+        except:
+            pass
+        finally:
+            return False
     
     def is_delicious(self):
         return True
@@ -50,19 +43,13 @@ class JellyBean(Dessert):
         self.set_flavor(flavor)
 
     def set_flavor(self,v):
-        if not isinstance(v, str):
-            if v != None:
-                print("Flavor should be a string. Flavor set to None")
-            self._flavor = None
-        else:
-            self._flavor = v
+        self.flavor = v
 
     def get_flavor(self):
-        return self._flavor
+        return self.flavor
     
     def is_delicious(self):
-        if self._flavor == 'black licorice':
+        if self.flavor == 'black licorice':
             return False
         
         return True
-    
